04af42364265348f446588eda36aad23a023baf4
justen <justen@justens-MacBook-Pro.local>
2019-03-05 10:38:22 -1000

    almost done

---
+++ normal_cars.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS normal_cars;
+DROP USER IF EXISTS normal_user;
+CREATE USER normal_user;
+CREATE DATABASE normal_cars WITH OWNER normal_user;
+\timing
+\c normal_cars;
+\i scripts/denormal_data.sql;
+CREATE TABLE makeTable(
+    ID SERIAL NOT NULL PRIMARY KEY,
+    make_code character varying(125) NOT NULL,
+    make_title character varying(125) NOT NULL
+);
+INSERT INTO makeTable (make_code, make_title)
+SELECT DISTINCT make_code, make_title FROM car_models;
+
+CREATE TABLE modelTable(
+    ID SERIAL NOT NULL PRIMARY KEY,
+    model_code character varying(125) NOT NULL,
+    model_title character varying(125) NOT NULL
+);
+INSERT INTO modelTable (model_code, model_title)
+SELECT DISTINCT model_code, model_title FROM car_models;
+
+CREATE TABLE yearTable(
+    ID SERIAL NOT NULL PRIMARY KEY,
+    year integer NOT NULL
+);
+INSERT INTO yearTable (year)
+SELECT DISTINCT year FROM car_models ;
+
+SELECT DISTINCT make_code, make_title FROM car_models;
+SELECT DISTINCT model_code, model_title FROM car_models WHERE make_code = 'VOLKS';
+SELECT DISTINCT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+SELECT DISTINCT COUNT(*) FROM car_models WHERE year BETWEEN 2010 AND 2015;
